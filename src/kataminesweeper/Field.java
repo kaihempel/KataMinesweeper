f661d1d71180897822f0044ad1d3903d52dd6d02
Kai Hempel <dev@kuweh.de>
2016-06-22 22:27:32 +0200

    Doc blocks

--- src/kataminesweeper/Field.java
+++ src/kataminesweeper/Field.java
@@ -10,30 +10,59 @@ public class Field {
     int counter = 0;
     boolean hasBomb = false;
 
+    /**
+     * Empty field constructor
+     */
     public Field() {
     }
 
+    /**
+     * Bomb field constructor
+     *
+     * @param hasBomb
+     */
     public Field(boolean hasBomb) {
         this.hasBomb = hasBomb;
     }
 
+    /**
+     * Checks if the field has a bomb
+     *
+     * @return True if the field has a bomb
+     */
     public boolean hasBomb() {
         return this.hasBomb;
     }
 
+    /**
+     * Checks if the field is empty
+     *
+     * @return True if there is no bomb
+     */
     public boolean isEmpty() {
         return (this.hasBomb == false);
     }
 
+    /**
+     * Place a bomb
+     */
     public void setBomb() {
         this.counter = 0;
         this.hasBomb = true;
     }
 
+    /**
+     * Returns the number of bombs in the neighbourhood
+     *
+     * @return Number of bombs near to this field
+     */
     public int getCounter() {
         return this.counter;
     }
 
+    /**
+     * Increase the counter
+     */
     public void incrementCounter() {
         this.counter++;
     }
--- src/kataminesweeper/Playground.java
+++ src/kataminesweeper/Playground.java
@@ -14,17 +14,33 @@ public class Playground {
 
     Field[][] playground;
 
+    /**
+     * Constructor
+     *
+     * @param width
+     * @param height
+     */
     public Playground(int width, int height) {
         this.width      = width;
         this.height     = height;
         this.playground = new Field[width][height];
     }
 
+    /**
+     * Initialize the playground.
+     * Bombs will placed randomly!
+     */
     public void initialzePlayground() {
         Boolean placeBombs = true;
         this.initialzePlayground(placeBombs);
     }
 
+    /**
+     * Initialize the playground.
+     * Bomb placement can deactivated!
+     *
+     * @param placeBombs
+     */
     public void initialzePlayground(Boolean placeBombs) {
 
         for (int x = 0; x < this.width; x++) {
@@ -38,6 +54,11 @@ public class Playground {
         }
     }
 
+    /**
+     * Calculates the number of bombs based on the field size.
+     *
+     * @return Maximum number of bombs
+     */
     private int calculateNumberOfBombs() {
         int divisor;
         if (this.width < this.height) {
@@ -49,6 +70,9 @@ public class Playground {
         return Math.round((this.width + this.height) / divisor);
     }
 
+    /**
+     * Set the bombs on random positions
+     */
     private void placeBombs() {
         int numberOfBombs = this.calculateNumberOfBombs();
         int x = 0;
@@ -66,6 +90,12 @@ public class Playground {
         } while (numberOfBombs > 0);
     }
 
+    /**
+     * Updates the field counters of all neighbour fields of the placed bomb
+     *
+     * @param x
+     * @param y
+     */
     private void updateFieldCounters(int x, int y) {
 
         int startX  = this.getUpdateStartPosition(x);
@@ -76,8 +106,13 @@ public class Playground {
         this.incrementFieldCounterInRange(startX, startY, endX, endY);
     }
 
+    /**
+     * Returns the start position for counter update
+     *
+     * @param pos
+     * @return Start position; Zero is the minimal start position
+     */
     private int getUpdateStartPosition(int pos) {
-
         if (pos == 0) {
             return 0;
         }
@@ -85,6 +120,12 @@ public class Playground {
         return pos - 1;
     }
 
+    /**
+     * Returns the end position for counter update
+     *
+     * @param pos
+     * @return End position
+     */
     private int getUpdateEndPosition(int pos) {
         int endPos = pos + 1;
 
@@ -95,6 +136,14 @@ public class Playground {
         return endPos;
     }
 
+    /**
+     * Increments the counter of all bombfree fields
+     *
+     * @param startX
+     * @param startY
+     * @param endX
+     * @param endY
+     */
     private void incrementFieldCounterInRange(int startX, int startY, int endX, int endY) {
 
         for (int x = startX; x <= endX; x++) {
@@ -107,22 +156,50 @@ public class Playground {
         }
     }
 
+    /**
+     * Checks if the playfield size is zero
+     *
+     * @return True if playground size is zero
+     */
     public boolean isEmpty() {
         return (this.width == 0 || this.height == 0);
     }
 
+    /**
+     * Returns the width
+     *
+     * @return Playground width
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Returns the height
+     *
+     * @return Playground height
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Checks if the field has a bomb
+     *
+     * @param x
+     * @param y
+     * @return True if the field has a bomb
+     */
     public boolean hasBomb(int x, int y) {
         return this.playground[x][y].hasBomb();
     }
 
+    /**
+     * Place a bomb on the given position
+     *
+     * @param x
+     * @param y
+     */
     public void setBomb(int x, int y) {
         if (! this.isEmpty() && this.playground[x][y] != null) {
             this.playground[x][y].setBomb();
@@ -130,10 +207,23 @@ public class Playground {
         }
     }
 
+    /**
+     * Returns the playground data array
+     *
+     * @return Playground array
+     */
     public Field[][] getPlayground() {
         return this.playground;
     }
 
+    /**
+     * Returns the field counter
+     *
+     * @param x
+     * @param y
+     * @return Number of bombs in the neighbourhood
+     * @throws Exception
+     */
     public int getFieldCounter(int x, int y) throws Exception {
 
         if (this.playground[x][y].hasBomb()) {
